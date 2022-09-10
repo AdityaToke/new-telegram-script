9216b39b2f0b8153f49d0d35c0f8d0d815e29b1b
Aditya Toke <atoke@ischoolconnect.com>
2022-09-10 12:29:45 +0530

    removed unwanted mv command

--- load-angular-dashboard.sh
+++ load-angular-dashboard.sh
@@ -4,7 +4,6 @@ mkdir /Users/adityatoke/Documents/self\ project/new-telegram-script/dist/dashboa
 sed 's#src="#src="new-telegram-script/dashboard/#g' dist/my-stock-dashboard/index.html > dist/my-stock-dashboard/text.html
 sed 's#href="styles#href="new-telegram-script/dashboard/styles#g' dist/my-stock-dashboard/text.html > dist/my-stock-dashboard/index.html
 rm -rf dist/my-stock-dashboard/text.html
-mv dist/my-stock-dashboard/text.html dist/my-stock-dashboard/index.html
 cp -R /Users/adityatoke/Documents/self\ project/stock-chrome-ext/dist/my-stock-dashboard/ /Users/adityatoke/Documents/self\ project/new-telegram-script/dist/dashboard
 
 
