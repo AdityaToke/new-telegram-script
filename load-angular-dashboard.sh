ae9006e6712ffdaab785244f678f5699b58b41de
Aditya Toke <atoke@ischoolconnect.com>
2022-09-10 11:42:21 +0530

    [Build]

--- load-angular-dashboard.sh
+++ load-angular-dashboard.sh
@@ -1,3 +1,4 @@
 cd /Users/adityatoke/Documents/self\ project/stock-chrome-ext
 git pull && git fetch origin && yarn build
+cp -R /Users/adityatoke/Documents/self\ project/stock-chrome-ext/dist/my-stock-dashboard/index.html /Users/adityatoke/Documents/self\ project/new-telegram-script/dist/dashboard/
 rsync -av --progress /Users/adityatoke/Documents/self\ project/stock-chrome-ext/dist/my-stock-dashboard/ /Users/adityatoke/Documents/self\ project/new-telegram-script/dist/ --exclude index.html
